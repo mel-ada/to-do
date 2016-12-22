424583950c71941f142f40471c4d54675d385340
Melissa Morel <melmorel06@gmail.com>
2016-12-22 14:13:43 -0800

    added rearrange button

--- models/schema.sql
+++ models/schema.sql
@@ -4,5 +4,6 @@ CREATE TABLE listo (
     id SERIAL PRIMARY KEY,
     task VARCHAR(100) NOT NULL,
     complete BOOLEAN NOT NULL DEFAULT false,
-    checked BOOLEAN NOT NULL DEFAULT false
+    checked BOOLEAN NOT NULL DEFAULT false,
+    listOrder SERIAL
 )
